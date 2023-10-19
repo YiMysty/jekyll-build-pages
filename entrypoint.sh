87e44ac465b08178fd4fa9b6d75e03799f1e107b
Mingzi <yimysty@github.com>
2023-10-19 15:48:08 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -42,7 +42,7 @@ fi
 cd "$PAGES_GEM_HOME"
 
 echo "::warning:: start to build"
-$GITHUB_PAGES_BIN build "$VERBOSE" "$FUTURE" --source "$SOURCE_DIRECTORY" --destination "$DESTINATION_DIRECTORY" 2>&1
+output=$($GITHUB_PAGES_BIN build "$VERBOSE" "$FUTURE" --source "$SOURCE_DIRECTORY" --destination "$DESTINATION_DIRECTORY" 2>&1)
 
 # Capture the exit code in a variable
 exit_code=$?
