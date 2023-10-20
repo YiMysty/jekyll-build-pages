b2f68c3be4bdf7b7da778d8fa4d3383959dfd0a7
Mingzi <yimysty@github.com>
2023-10-19 17:14:11 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -49,7 +49,7 @@ build_output="$($GITHUB_PAGES_BIN build "$VERBOSE" "$FUTURE" --source "$SOURCE_D
 # Capture the exit code in a variable
 exit_code=$?
 
-encoded_string==$(echo "$build_output" | tr '\n' ' ')
+encoded_string==$(echo "$build_output" | tr '\n' '%0A')
 
 echo "::warning  $encoded_string"
 
